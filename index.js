b3915443c3e59d0ae760ced9dcb1b2f738230f3a
Liam Roberts <liam_r_123@hotmail.co.uk>
2018-11-12 13:28:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function updateObjectWithKeyAndValue(object, key, value){
   obj.prop2 = 2;
   return obj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  var obj = { prop: 1 };
+  destructivelyUpdateObjectWithKeyAndValue(obj, 'prop2', '2')
+  return obj;
+}
